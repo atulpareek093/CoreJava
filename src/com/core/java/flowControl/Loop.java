da1e33e1f1d59ab4bba38a63774d2c84ab187f6a
shakti <atulpareek093@gmail.com>
2020-12-21 10:55:08 +0530

    Loop code

---
+++ src/com/core/java/flowControl/Loop.java
@@ -0,0 +1,49 @@
+package com.core.java.flowControl;
+
+public class Loop {
+	public static void main(String args[])
+	{
+		Loop loop=new Loop();
+		loop.doWhile();
+	}
+	public void whileLoop()
+	{
+		int a=10;
+		int i=1;
+		while(i<=a)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+	
+	public void forLoop()
+	{
+		int a=10;
+		for(int i=1; i<=a;i++)
+		{
+			System.out.println(i);
+		}
+	}
+	
+	public void enhanceFor()
+	{
+		int a[]= {1,2,3,4,5};
+		
+		for(int b:a)
+		{
+			System.out.println(b);
+		}
+	}
+	public void doWhile()
+	{
+		int a=10;
+		int i=10;
+
+		do {
+			
+		System.out.println("a");
+	}while(i<a);
+	}
+	 
+}
--- src/com/core/test/Test1.java
+++ src/com/core/test/Test1.java
@@ -1,20 +1,61 @@
 package com.core.test;
 
 public class Test1 {
-public static void main(String arg[])
-{
-	
-	for(int i=0; i<=10 ; i++)
-	{
+	public static void main(String arg[]) {
+		 int n1=3;int n2=1;int  n3=5;int n4=7;int a=1;
+		for(int i=1;i<n3; i++)
+		{	
 		
-		
-		if(i==3)
+			for(int j =n1; j>=i; j--)
+			{
+				System.out.print("  ");
+			}
+		for(int j =1; j<=n2; j++)
 		{
-			continue;
-			//System.out.println(i);
+			
+			if(j==1 || j==n2)
+			{
+				System.out.print("* ");
+				
+			}
+			else
+			{
+				System.out.print("  ");
+			}
 		}
-		System.out.println(i);
+		n2=n2+2;
+		System.out.println();
+			 
+		}
 		
+		
+		
+		for(int i=1; i <n3-1;i++)
+		{
+			
+			for(int j=1;j<=i; j++)
+			{
+			System.out.print("  ");
+			}
+			for(int j=n3;j>=a; j--)
+			{
+				if(j==n3 || j==a)
+				{
+					System.out.print("* ");
+					
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			 a=a+2;
+			System.out.println();
+		}
+		 
 	}
-}
+
+	 
+
+
 }
